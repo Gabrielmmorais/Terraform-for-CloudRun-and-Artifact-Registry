30fd813a22df49a5818ed928bc1690537a17826f
Gabriel Morais <gabrielmacielm@gmail.com>
2024-08-27 00:01:09 -0300

    Update: Update docker build

--- TF_CloudRun_AR/scrapper1/push.tf
+++ TF_CloudRun_AR/scrapper1/push.tf
@@ -2,7 +2,7 @@ resource "null_resource" "push_docker_image" {
   provisioner "local-exec" {
     command = <<-EOT
       gcloud auth configure-docker us-west1-docker.pkg.dev
-      docker build -t scrapper1:v1 -f ./app/Dockerfile
+      docker docker buildx build --platform linux/amd64 -t scrapper1:v1 -f scrapper1/app/Dockerfile .
       docker tag scrapper1:v1 us-west1-docker.pkg.dev/your_project_id/scrapper1/scrapper1:your_tag
       docker push us-west1-docker.pkg.dev/your_project_id/scrapper1/scrapper1:your_tag
     EOT
--- TF_CloudRun_AR/scrapper2/push.tf
+++ TF_CloudRun_AR/scrapper2/push.tf
@@ -2,7 +2,7 @@ resource "null_resource" "push_docker_image" {
     provisioner "local-exec" {
     command = <<-EOT
       gcloud auth configure-docker us-west1-docker.pkg.dev
-      docker build -t scrapper2:v1 -f ./app/Dockerfile
+      docker docker buildx build --platform linux/amd64 -t scrapper2:v1 -f scrapper2/app/Dockerfile .
       docker tag scrapper2:v1 us-west1-docker.pkg.dev/your_project_id/scrapper2/scrapper2:your_tag
       docker push us-west1-docker.pkg.dev/your_project_id/scrapper2/scrapper2:your_tag
     EOT
